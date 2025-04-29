dcbe67a212ec20315bab4b8d41ffdce72f881312
grpCondusef <grpcondusef@gmail.com>
2025-04-29 16:08:16 -0600

    changed server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ dotenv.config();
 
 import app from './app';
 
-const PORT = process.env.PORT || 3001;
+const PORT = parseInt(process.env.PORT || '3001', 10);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
